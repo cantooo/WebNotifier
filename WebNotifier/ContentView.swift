51be8cd07ba8699825de901b18020314691f80eb
Simone Cantoni <simone.cant@icloud.com>
2021-04-20 13:55:08 +0200

    Initial commit (input and delete into list without save)

---
+++ WebNotifier/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  WebNotifier
+//
+//  Created by Simone Cantoni on 05/04/21.
+//
+
+import SwiftUI
+
+struct Url: Identifiable, Hashable {
+    var url:String
+    var id = UUID()
+}
+
+struct ContentView: View {
+    @State var input:String = ""
+    @State var urls:[Url] = []
+    
+    var body: some View {
+        NavigationView(content: {
+            Form(content: {
+                Section(header: Text("INSERIMENTO")) {
+                    TextField("URL da controllare", text: $input)
+                    Button("Inizia a controllare") {
+                        urls.append(Url(url: input))
+                        input = ""
+                    }
+                }
+                Section(header: Text("CONTROLLO IN CORSO")) {
+                    List{
+                        ForEach(urls, id: \.self) { e in
+                            Text(e.url)
+                        }.onDelete(perform: { indexSet in
+                            urls.remove(atOffsets: indexSet)
+                        })
+                    }
+                }
+            })
+            .navigationTitle("WebNotifier")
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebNotifier/WebNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebNotifierApp.swift
+//  WebNotifier
+//
+//  Created by Simone Cantoni on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct WebNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
