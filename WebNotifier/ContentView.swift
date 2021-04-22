14de441860673346f736d972a31f1d5d588307ab
Simone Cantoni <simone.cant@icloud.com>
2021-04-22 11:48:04 +0200

    When the user adds an url, the webpage is fetched to ensure that it exists, and if not an error alert appears.

--- WebNotifier/ContentView.swift
+++ WebNotifier/ContentView.swift
@@ -13,6 +13,7 @@ let urlsKey = "WebNotifierUrls"
 struct ContentView: View {
     @State var input:String = ""
     @State var urls:[String] = userDefaults.object(forKey: urlsKey) as? [String] ?? []
+    @State var alert = false
     
     var body: some View {
         NavigationView(content: {
@@ -20,12 +21,19 @@ struct ContentView: View {
                 Section(header: Text("INSERIMENTO")) {
                     TextField("URL da controllare", text: $input)
                     Button("Inizia a controllare") {
-                        let url = input.trimmingCharacters(in: .whitespacesAndNewlines)
+                        let url = input.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+                        
                         if !url.isEmpty {
-                            urls.append(url)
-                            userDefaults.set(urls, forKey: urlsKey)
+                            if !checkWebpage(at: url) {
+                                alert.toggle()
+                            } else {
+                                urls.append(url)
+                                userDefaults.set(urls, forKey: urlsKey)
+                            }
                         }
                         input = ""
+                    }.alert(isPresented: $alert) {
+                        Alert(title: Text("URL errato"), message: Text("L'URL fornito non sembra corrispondere a un URL valido."), dismissButton: .default(Text("Ok")))
                     }
                 }
                 Section(header: Text("CONTROLLO IN CORSO")) {
@@ -41,6 +49,31 @@ struct ContentView: View {
             })
             .navigationTitle("WebNotifier")
         })
+    }
+    
+    func checkWebpage (at url:String)->Bool {
+        let html = fetchHTML(at: url) ?? ""
+        
+        if html != "" {
+            return true
+        }
+        
+        return false
+    }
+    
+//  Fetches the HTML of the webpage at the given url
+    func fetchHTML (at url: String)->String? {
+        var html = ""
+        
+        do {
+            html = try String(contentsOf: URL(string: url)!, encoding: .ascii)
+        } catch let error {
+            print(error.localizedDescription)
+            print("Non sono riuscito a caricare la pagina", url, ".")
+            return nil
+        }
+        
+        return html
     }
 }
 
